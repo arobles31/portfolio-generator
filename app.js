ba37ba76d3f3e6ca22974806a166f92a5bf96c1c
Alejandro Robles <arobles2828@gmail.com>
2021-04-10 22:50:52 -0700

    created HTML code for portfolio

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -133,12 +136,15 @@ Add a New Project
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData)
+   const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
 });
 
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
     
